3acf6ff37d53dd3154f643b67248e42c1d0fa6a6
sarbajitdutta <sarbajitdutta35@gmail.com>
2017-08-30 11:09:07 -0400

    deleted

--- modules/vivodashboard/citations/js/faq.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-
-    Drupal.behaviors.violin = { attach: function (select, settings) {
-
-        var linksToMain = jQuery('a[href="/citations/main"]');
-        linksToMain.addClass("active");
-
-    } }
-})(jQuery);
